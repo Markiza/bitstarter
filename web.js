0df65ee88a4424449b667d2d6f543ece7520ed9a
Markiza <egolubzova@gmail.com>
2013-07-10 18:43:05 +0000

    Web with readfilesync

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
 
+var fs = require ('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    response.send('Hello World from index.html');
 });
+
+fs.readFileSync('index.html');
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
