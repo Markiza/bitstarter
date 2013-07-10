3a85ac40fec76173166110fe277d250c62599fb5
Markiza <egolubzova@gmail.com>
2013-07-10 19:32:02 +0000

    Index new

--- web.js
+++ web.js
@@ -4,14 +4,13 @@ var fs = require ('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   response.send('Hello World from index.html');
+   response.send(fs.readFileSync('index.html'));
 });
 
-fs.readFileSync('index.html');
+//fs.readFileSync('index.html');
 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
    console.log("Listening on " + port);
 });
-
