e107bf9952c800206edc8043608d173b5bc253e1
Markiza <egolubzova@gmail.com>
2013-07-10 18:01:25 +0000

    last

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var fs = require('fs');
+var express = require('express');
 
+var app = express.createServer(express.logger());
 
-fs.writeFile('index.html', 'Hello node',function (err) {
-if (err) throw err;
-console.log('It\'s saved!');
+app.get('/', function(request, response) {
+   response.send('Hello World from index.html');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+   console.log("Listening on " + port);
 });
